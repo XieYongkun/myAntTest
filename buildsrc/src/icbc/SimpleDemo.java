02163b5689436bbfe3a39f4871eeb1fad8fedbcc
admin <332625228@qq.com>
2019-07-15 18:59:15 +0800

    new ant

---
+++ buildsrc/src/icbc/SimpleDemo.java
@@ -0,0 +1,13 @@
+package icbc;
+
+public class SimpleDemo {
+	 public int Add(int a,int b){        
+	    return (a+b);        
+	}
+	 public int Method(int a){  
+		 if(a==8){
+			 return (a);    
+		 }
+		    return (a);        
+		}
+}
