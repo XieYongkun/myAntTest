30ebccda4e8d49bf61f6182562add9ff84545109
admin <332625228@qq.com>
2019-07-17 16:55:58 +0800

    ceshi

---
+++ buildsrc/src/test/SimpleDemoTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import icbc.SimpleDemo;
+
+public class SimpleDemoTest {
+
+	SimpleDemo sc = new SimpleDemo();
+	     @Test
+	    public void AddTest() {
+	         int c = sc.Add(3, 5);    
+	         assertEquals(8, c);        
+	     }
+	     @Test
+		    public void MethodTest() {
+		         int d = sc.Method(8);
+		         assertEquals(8, d);        
+		     }
+}
